6e555afa6b5acfda51eae4038acd3c79d8a6fc1f
Vitor Hugo <vitor.ribeirohugo88@gmail.com>
2026-09-03 14:06:15 -0300

    Add Google Tag Manager container

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,8 +47,26 @@ export default function RootLayout({
             gtag('config', 'AW-18427985890');
           `}
         </Script>
+        <Script id="google-tag-manager" strategy="beforeInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-P88G5NDM');
+          `}
+        </Script>
       </head>
       <body className="antialiased">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-P88G5NDM"
+            height="0"
+            width="0"
+            title="Google Tag Manager"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         {children}
         <WhatsAppFloating />
       </body>
